b19f9f26e35417d923a68e462ea5ee4f9f3ab389
Justin Smith <124634162+smithjustinc@users.noreply.github.com>
2025-02-23 14:01:29 -0500

    Island Perimeter java file second commit

--- src/IslandPerimeter.java
+++ src/IslandPerimeter.java
@@ -55,7 +55,6 @@
         }
     public static void main(String[] args) {
         int[][] x = {{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}};
-        //, {1, 1, 0, 0}
         System.out.println(islandPerimeter(x));
     }
 }
