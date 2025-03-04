c18e4eb302ef41cf8005fcad15ec4af0444594fc
Justin Smith <124634162+smithjustinc@users.noreply.github.com>
2025-03-04 13:41:52 -0500

    PowerOfFour java file

---
+++ src/PowerOfFour.java
@@ -0,0 +1,21 @@
+/*
+ * Given an integer n, return true if it is a power of four. Otherwise, return false.
+ * An integer n is a power of four, if there exists an integer x such that n == 4^x.
+ */
+
+public class PowerOfFour {
+    public static boolean isPowerOfFour(int n) {
+        if (n == 0) return false;
+        while (n % 4 == 0) {
+            n = n/4;
+        }
+        if (n == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
